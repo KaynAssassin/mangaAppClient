ff5105011cb251b9633addf4cc8e8482ca422553
jhin <allmid66@gmail.com>
2020-09-01 18:07:50 +0700

    init app

---
+++ screens/ListCategoryManga.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text,StyleSheet,View} from 'react-native';
+
+export default function ListCategoryManga(props){
+    return <View>
+            <Text>Hello</Text>
+        </View>
+    
+}
